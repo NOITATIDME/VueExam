137aa72bbc69683470606190930fb3f467d312f3
NOITATIDME <chgt186@gmail.com>
2022-06-19 15:11:46 +0900

    vue 연습

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }}</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }}</p>
+  </div>
+
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      price1: 50,
+      price2: '가격은 아무거나',
+      스타일: 'color: blue',
+      products: ['역삼동원룸', '마포구원룸', '천호동원룸']
+    }
+  },
+
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
